cf2c2236c8b413e151d5c24b15ca34c0398aca71
Gil Ferreira <gfgilly@gmail.com>
2015-07-13 19:32:32 -0400

    Initial Commit

---
+++ MemeMe/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MemeMe
+//
+//  Created by Gil Ferreira on 7/13/15.
+//  Copyright (c) 2015 Gil Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
